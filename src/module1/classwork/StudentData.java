03241029fae9708cf284fb593821742898e892d2
Abhijit Smiju Kunnel <abhijit.uni29@gmail.com>
2026-08-01 09:53:09 +0530

    feat: Add Classwork

---
+++ src/module1/classwork/StudentData.java
@@ -0,0 +1,44 @@
+package module1.classwork;
+import java.util.Scanner;
+
+class ConfidentialData{
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String myname) {
+		name = myname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int myage) {
+		if (myage > 0) {
+			age = myage;
+		}
+	}
+		
+}
+
+public class StudentData {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ConfidentialData student = new ConfidentialData();
+        
+        System.out.print("Enter student's name: ");
+        String name = sc.nextLine();
+        student.setName(name);
+        
+        System.out.print("Enter student's age: ");
+        int age = sc.nextInt();
+        student.setAge(age);
+        
+        System.out.println("\nStudent Data");
+        System.out.println("Name: " + student.getName());
+        System.out.println("Age: " + student.getAge());
+        
+        sc.close();
+    }
+}
