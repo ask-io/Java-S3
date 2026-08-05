40666df30fe9c9d148e79777b729d43a462a29e5
Abhijit Smiju Kunnel <abhijit.uni29@gmail.com>
2026-08-05 09:47:58 +0530

    feat: Add more classwork

---
+++ src/module1/classwork/FactorialDemo.java
@@ -0,0 +1,28 @@
+package module1.classwork;
+import java.util.Scanner;
+
+class Factorial {
+    int fact(int n) {
+ 
+        if (n <= 1) {
+            return 1;
+        }
+        int result = n * fact(n - 1);
+        return result;
+    }
+}
+
+public class FactorialDemo {
+
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        Factorial f = new Factorial();
+        
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int result = f.fact(number);
+        
+        System.out.println("Factorial of " + number + " is: " + result);
+        sc.close();
+    }
+}
