68b87c5739f36777e37913b97a9bd4b65cc1ef13
Abhijit Smiju Kunnel <abhijit.uni29@gmail.com>
2026-08-05 09:47:58 +0530

    feat: Add more classwork

---
+++ src/module1/classwork/FibonacciDemo.java
@@ -0,0 +1,30 @@
+package module1.classwork;
+import java.util.Scanner;
+
+class Fibonacci {
+	int n3 = 0, n2 = 1, n1 = 0;
+	void fib(int n){
+		if(n>0) {
+			n3 = n1+n2;
+			n1 = n2;
+			n2 = n3;
+			System.out.print(" " + n3);
+			fib(n-1);
+		}
+	}
+}
+
+public class FibonacciDemo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Fibonacci f = new Fibonacci();
+        
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        f.fib(n);
+        sc.close();
+
+	}
+
+}
