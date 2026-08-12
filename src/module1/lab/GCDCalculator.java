5f6b85aee9e28e2251b39af2a1d1dfc2cbeeacb5
Abhijit Smiju Kunnel <abhijit.uni29@gmail.com>
2026-08-12 11:56:36 +0530

    feat: Add GCD Calculator solution

--- src/module1/lab/GCDCalculator.java
+++ src/module1/lab/GCDCalculator.java
@@ -1,5 +1,22 @@
 package module1.lab;
+import java.util.Scanner;
 
 public class GCDCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
 
+        int originalA = a, originalB = b;
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD of " + originalA + " and " + originalB + " is: " + a);
+        sc.close();
+    }
 }
